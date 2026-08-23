fc78eb8d1079b40d043c338a4c3876320b5c1f35
Kytolly <1392875097@qq.com>
2026-08-23 17:24:07 +0800

    refactor: remove legacy egoexo_translation_bench package structure

--- egoexo_translation_bench/dataflow/__init__.py
+++
@@ -1,6 +0,0 @@
-from .submission import Submission
-from .option import Options
-from .set import BenchmarkDataset
-from .loader import BenchmarkDataLoader
-
-__all__ = ['Submission', 'Options', 'BenchmarkDataset', 'BenchmarkDataLoader']
